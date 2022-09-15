48eac8e96e88c3d6032039f33c1f2a706d3183f5
WicasZ <williamhumbertozi@hotmail.com>
2022-09-15 02:39:23 -0500

    finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MaincuerpoComponent } from './maincuerpo/maincuerpo.component';
+import { FeaturesComponent } from './features/features.component';
+import { ToolComponent } from './tool/tool.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MaincuerpoComponent,
+    FeaturesComponent,
+    ToolComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/maincuerpo/maincuerpo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maincuerpo',
+  templateUrl: './maincuerpo.component.html',
+  styleUrls: ['./maincuerpo.component.css']
+})
+export class MaincuerpoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
